e5f03c23b088a9ed986f00f49f9575d8c8cf7abe
Dat Le Tran <letd@garena.com>
2016-12-15 11:50:50 +0800

    router: components: create router to PhotoGrid and Single

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const PhotoGrid = React.createClass({
+	render() {
+		return (
+			<div className="Photo-grid">
+				Photo-grid
+			</div>
+		);
+	}
+})
+
+export default PhotoGrid
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Single = React.createClass({
+	render() {
+		return (
+			<div className="Single-photo">
+				Single
+			</div>
+		);
+	}
+})
+
+export default Single
